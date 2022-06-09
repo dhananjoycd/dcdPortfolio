ca2e2623801b3337a2374635e9598db56c3455b5
dhananjoycd <dcdmf2020@gmail.com>
2022-06-09 22:50:07 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Components/About/About';
+import Blogs from './Components/Blogs/Blogs';
+import ContactForm from './Components/ContactForm/ContactForm';
+import Home from './Components/Home/Home';
+import Projects from './Components/Projects/Projects';
+import Resume from './Components/Resume/Resume';
+
+function App() {
+  return (
+    <div>
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="blogs" element={<Blogs/>} />
+        <Route path="resume" element={<Resume/>} />
+        <Route path="projects" element={<Projects/>} />
+        <Route path="contact" element={<ContactForm/>} />
+        <Route path="about" element={<About/>} />
+    </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ContactForm/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactForm = () => {
+    return (
+        <div>
+            contact
+        </div>
+    );
+};
+
+export default ContactForm;
